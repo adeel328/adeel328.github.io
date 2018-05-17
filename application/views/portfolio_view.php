24228c2b0a814005838a8f810eb32048051c9fb6
adeel328 <adeelhashmi328@gmail.com>
2018-05-17 16:15:30 +0500

    first commit

---
+++ application/views/portfolio_view.php
@@ -0,0 +1,501 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>porfolio</title>
+    <link rel="icon" href="assests/img/bt.png" type="image/png" sizes="16x16">
+    <link href="assests/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+   
+    <link href="assests/css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body>
+
+
+		<div class="head ha2">
+			<p>Porfolio</p>
+		</div>
+
+	<div class="mg_relative">
+	<div class="fimg">
+		<div class="ab-image-shado"></div>
+
+		<img class="adeelmg" name="slide" width="400" height="200">
+		<!-- <img class="" src=""> -->
+		
+		</div>
+		<div class="name">
+	<p>--------Welcome</p>
+	<h1><b> <i> I am Muhamamd Adeel </i></b></h1>
+	<h3><b> Web Developer </b></h3>
+	</div>
+	</div>	
+		
+		<div class="hading_my_self">
+			<h1> <b>My Self </b></h1>
+		</div>
+		<div class="wh">
+		<div class="container">
+			<div class="row mr">
+				<div class="col-md-5">
+					<div class="self">
+							<div class="selftxt">
+						<h1>Name</h1>
+						<p>
+							Muhammad Adeel
+						</p>
+						<hr>
+						<h1>Sex</h1>
+						<p>Male</p>
+						<hr>
+						<h1>Address</h1>
+						<p>Chak no 328GB, Tehsil Pirmahal, District Toba Tek Singh</p>
+						<hr>
+						<h1>Phone Number</h1>
+						<p>+92-300-5398878</p>
+						<hr>
+						<h1>Email Address</h1>
+						<p>adeelhashmi328@gmail.com</p>
+						<hr>
+						</div>
+					</div>
+					
+				</div>
+				<div class="col-md-7">
+					<div class="selfborder">
+					<img class="self_mg" src="assests/img/adeel.jpg">
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		</div>
+
+		<!-- skills -->
+		<div class="dist"></div>
+		<div class="simg_relative">
+			
+		
+		<div class="simg">
+			<div class="simg-image-shado"></div>
+
+			<div class="skl">
+			<h1>My Skills</h1>
+			</div>
+			<div class="skillbar" data-percent="76">
+			  <span class="skillbar-title" style="background: #a0d034;">HTML</span>
+			  <p class="skillbar-bar" style="background: rgb(178, 236, 47); width: 76%;"></p>
+			  <span class="skill-bar-percent">76%</span>
+			</div>
+			<div class="skillbar" data-percent="78">
+			  <span class="skillbar-title" style="background: #f1703a;">Wordpress</span>
+			  <p class="skillbar-bar" style="background: rgb(248, 140, 94); width: 70%;"></p>
+			  <span class="skill-bar-percent">70%</span>
+			</div>
+			<div class="skillbar" data-percent="94">
+			  <span class="skillbar-title" style="background: #2980b9;">JavaScript</span>
+			  <p class="skillbar-bar" style="background: rgb(52, 152, 219); width: 65%;"></p>
+			  <span class="skill-bar-percent">65%</span>
+			</div>
+			<div class="skillbar" data-percent="54">
+			  <span class="skillbar-title" style="background: #f1703a;">Photoshop</span>
+			  <p class="skillbar-bar" style="background: rgb(248, 140, 94); width: 50%;"></p>
+			  <span class="skill-bar-percent">50%</span>
+			</div>
+			<div class="skillbar" data-percent="64">
+			  <span class="skillbar-title" style="background: #2980b9;">CSS3</span>
+			  <p class="skillbar-bar" style="background: rgb(52, 152, 219); width: 64%;"></p>
+			  <span class="skill-bar-percent">64%</span>
+			</div>
+			<div class="skillbar" data-percent="84">
+			  <span class="skillbar-title" style="background: #a0d034;">PHP</span>
+			  <p class="skillbar-bar" style="background: rgb(178, 236, 47); width: 65%;"></p>
+			  <span class="skill-bar-percent">65%</span>
+			</div>
+			</div>
+		</div>
+
+
+				<!-- My Services  -->
+
+				<div class="my_services">
+					<h1> <b>My Services </b></h1>
+				</div>
+				
+	<div class="serv">
+				<div class="container">
+					<div class="row srv_dist">
+						<div class="col-md-1"></div>
+						<div class="col-md-3 cnt">
+							<div class="icon">
+							<i class="fa fa-laptop" aria-hidden="true"></i>
+							</div>
+							
+								<h2 >Web Design </h2>
+								
+							
+						
+						</div>
+						<div class="col-md-1"></div>
+						<div class="col-md-3 cnt">
+							<div class="icon">
+							<i class="fa fa-clone" aria-hidden="true"></i>
+						</div>
+							<h2>Creative Design</h2>
+						</div>
+						<div class="col-md-1"></div>
+						<div class="col-md-3 cnt">
+							<div class="icon">
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+							<h2>Html Coding</h2>
+						</div>
+
+					</div>
+				</div>
+</div>
+
+
+
+
+		<!-- Interests -->
+
+
+
+		<div class="interst_relative">
+			<div class="interst_mg">
+				
+				<div class="interst-image-shado"></div>
+				<div class="interst">
+				<h1><b> interests </b></h1>
+			</div>
+			<!-- <i class="fa fa-camera" style="font-size:48px;color:red"></i> -->
+			<div class="container">
+		
+		<div class="agile-w3l-in">
+			<i class="fa fa-camera icon_font" aria-hidden="true"></i>
+			<p class="p_margin">PHOTOGRAPHY</p>
+		</div>
+		<div class="agile-w3l-in">
+			<i class="fa fa-plane icon_font" aria-hidden="true"></i>
+			<p class="p_margin">TRAVELLING</p>
+		</div>
+		<div class="agile-w3l-in">
+			<i class="fa fa-book icon_font " aria-hidden="true"></i>
+			<p class="p_margin">Reading</p>
+		</div>
+		<div class="agile-w3l-in w3l-intra-re">
+			<i class="fa fa-gamepad icon_font" aria-hidden="true"></i>
+			<p class="p_margin">GAMING</p>
+		</div>
+		<div class="agile-w3l-in w3l-intra-re">
+			<i class="fa fa-music icon_font" aria-hidden="true"></i>
+			<p class="p_margin">MUSIC</p>
+		</div>
+	
+	</div>
+			</div>
+			
+
+			
+
+		</div>
+
+
+
+		<!-- Work Experiance  -->
+
+		
+		<div class="work_exp">
+			<h1><b> Work Experience </b></h1>
+		
+		</div>
+		<div class="wh">
+				<div class="container">
+		
+		<div class="wthree_latest_albums_grids">
+			<div class="cntl"> <span class="cntl-bar cntl-center"> <span class="cntl-bar-fill" style="height: 1209.52px;"></span> </span>
+				<div class="cntl-states">
+					<div class="cntl-state cntl-animate">
+						<div class="cntl-content">
+							<h4 class="h4cl">2018 Present</h4>
+							<p>PHP Codeignater Developer</p>
+						</div>
+						<div class="cntl-image">
+							<img src="assests/img/of4.jpg" alt=" " class="img-responsive1">
+							
+								
+							
+						</div>
+						<div class="cntl-icon cntl-center">01</div>
+					</div>
+					<div class="cntl-state cntl-animate">
+						<div class="cntl-content">
+							<h4 class="h4cl">2017</h4>
+							<p>FRONT END DEVELOPER</p>
+						</div>
+						<div class="cntl-image w3_cntl_image"><img src="assests/img/of5.jpg" alt=" " class="img-responsive1"></div>
+						<div class="cntl-icon cntl-center">02</div>
+					</div>
+					<div class="cntl-state cntl-animate">
+						<div class="cntl-content">
+							<h4 class="h4cl">2016</h4>
+							<p>Microsoft Office &amp; IT Solutions</p>
+						</div>
+						<div class="cntl-image">
+							<img src="assests/img/of3.jpg" alt=" " class="img-responsive1">
+							
+						</div>
+						<div class="cntl-icon cntl-center">03</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wh">
+<div class="work_exp">
+			
+		
+		</div>
+
+			<!-- Objectivs -->
+
+			<div class="obj_relative">
+			<div class="obj_mg ">
+				
+				<div class="obj-image-shado"></div>
+				<div class="obj">
+				<h1><b> Objective</b></h1>
+			</div>
+		<p class="p_txt">It's my pleasure to introduce my self..well, I'm Muhammad Adeel and raised in Pakistan.</p>
+		<p class="p_txt">I am looking forward to join a progressive organization. I have high level of personal morals and integrity. I am Goal oriented, self-motivated and committed to the successful outcome of the project. I am willing to work hard and have a great desire to learn. Seeking a position to best utilize my abilities and hard work in IT field that offers professional growth and learning experience while being innovative and flexible.</p>
+			
+			</div>
+			
+
+			
+
+		</div>
+
+
+
+			
+			<!-- Project -->
+
+
+			<div class="pro">
+			<h1><b>Project</b></h1>
+			</div>
+			<div class="wh">
+					<div class="container ">
+						<div class="row pr  ">
+							<div class="col-md-6 nopad">
+								<img class="img-responsive" src="assests/img/1.jpg">
+							</div>
+							<div class="col-md-6 pc w3_bl">
+								<div class="brline2">
+									<div class="pc">
+								<h1>Final Year Project</h1>
+								<h3>Kids Hub; Kids Learning Game</h3>
+								<p>Kids Hub features a delightfully multi-color design in Unity2d to attract kids and make it easier for them to receive information and develop their abilities without getting bored. It will play an important role in the beginning of basic learning of kids as; it is also a brain exercise for them.</p>
+								</div>
+							</div>
+
+
+							</div>
+							<div class="row pr nopad ">
+
+							<div class="col-md-6 ">
+								<div class="brline">
+								<div class="pc2">
+								<h1>Real Estate Bootstrap Responsive Web Template</h1>
+								<p>To be successful in real estate, you must always and consistently put your clients' best interests first. When you do, your personal needs will be realized beyond your greatest expectations.</p>
+								</div>
+								</div>
+							</div>
+							<div class="col-md-6 nopad">
+								<img class="img-responsive" src="assests/img/re.png">
+							</div>
+						</div>
+						</div>
+					</div>
+				</div>
+
+			
+
+
+
+
+		<div class="pro">
+			
+			</div>
+
+				<!-- education -->
+
+
+
+				<div class="edu_relative">
+			<div class="edu_mg ">
+				
+				<div class="edu-image-shado"></div>
+				<div class="edu">
+				<h1><b> Educations</b></h1>
+			</div>
+			<div class="wh">
+				<div class="container">
+					<div class="row ed_txt">
+						<div class="col-md-1"></div>
+						<div class="col-md-4">
+							<h2 class="h_col"> 2012 —2016 </h2>
+							<br>
+							<br>
+							<h2 class="h_col">2010 —2012</h2>
+							<br>
+							<br>
+							<h2 class="h_col">2008 —2010</h2>
+						</div>
+							<div class="col-md-7">
+								<h2 class="h2_col"> BS Software Engineering</h2>
+							
+							<h3 class="h3_col">University of Management and Technology Lahore</h3>
+							
+							<h2 class="h2_col"> F.S.c (Pre Engineering.)</h2>
+							<h3 class="h3_col">B.I.S.E (faisalabad)</h3>
+							
+							<h2 class="h2_col"> Matriculation (Science)</h2>
+							<h3 class="h3_col">B.I.S.E (faisalabad)</h3>
+						</div>
+						
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+		<div class="contact">
+		<h1><b> Contact Me </b></h1>
+		</div>
+
+		<div class="wh">
+		<div class="container">
+		
+		<div class="address-row">
+			<div class="col-md-6 address-left wow agile fadeInLeft animated" data-wow-delay=".5s">
+				<div class="address-grid">
+					<h4><b>Get In Touch</b></h4>
+					<form action="#" method="post">
+						<input type="text" placeholder="Name" name="name" required="">
+						<input type="email" placeholder="Email" name="email" required="">
+						<input type="text" placeholder="Subject" name="subject" required="">
+						<textarea placeholder="Message" required=""></textarea>
+						<input type="submit" value="SEND">
+					</form>
+				</div>
+			</div>
+			<div class="col-md-6 address-right">
+				<div class="address-info wow fadeInRight animated" data-wow-delay=".5s">
+					<h4>Address</h4>
+					<p>Chak no 328GB, Tehsil Pirmahal, District Toba Tek Singh</p>
+				</div>
+				<div class="address-info address-mdl wow fadeInRight animated" data-wow-delay=".7s">
+					<h4>Phone </h4>
+					<p>+92 300 5398878</p>
+					<p>+92 300 5398878</p>
+					
+				</div>
+				<div class="address-info agileits-info wow fadeInRight animated" data-wow-delay=".6s">
+					<h4>Mail</h4>
+					<p><a href="">adeelhashmi328@gmail.com</a></p>
+					<p><a href="">adeelhashmi328@gmail.com</a></p>
+					
+				</div>
+			</div>
+		</div>	
+	</div>
+</div>
+
+
+			<div class="contact">
+			
+				</div>
+					<!-- Footer -->
+				<div class="ftr">
+				<div class="container">
+				<div class="row">
+				<div class="col-md-6 "> 
+				<p class="ft_text">© 2018 Portfolio. All rights reserved | Design by &nbsp;</p> <p class="ft_text1"> <b>Muhammad Adeel</b></p>
+			</div>
+					  
+					
+			
+					
+				<div class="col-md-6 ft_text2"> 
+					<div class="w3ls-social-icons-2">
+			<a class="facebook" href="https://www.facebook.com/adeelhashmi435"><i class="fa fa-facebook"></i></a>
+			<a class="twitter" href="https://twitter.com/adeelhashmi328"><i class="fa fa-twitter"></i></a>
+			
+			<a class="linkedin" href="https://www.linkedin.com/in/muhammad-adeel-279b0584/?midToken=AQFqHTI0B45MoA&trk=eml-jobs_jymbii_digest-header-90-profile&trkEmail=eml-jobs_jymbii_digest-header-90-profile-null-4zeuyl~jh7x9fea~t1-null-neptune%2Fprofile~vanity%2Eview&lipi=urn%3Ali%3Apage%3Aemail_jobs_jymbii_digest%3BHItI6a7rSt2fXBf7rERSNA%3D%3D"><i class="fa fa-linkedin"></i></a>
+			
+		</div>				
+				</div>
+				</div>
+				</div>
+				</div>
+
+
+
+<script>
+	var i = 0; // Start point
+	var images = [];
+	var time = 2000;
+
+	// Image List
+	images[0] = 'assests/img/adeel3.jpg';
+	images[1] = 'assests/img/adeel4.jpg';
+	images[2] = 'assests/img/adeel5.jpg';
+	images[3] = 'assests/img/adeel/a.jpg';
+
+	
+	
+	images[4] = 'assests/img/adeel/a7.jpg';
+	images[5] = 'assests/img/adeel/a8.jpg';
+	
+	images[6] = 'assests/img/adeel/a10.jpg';
+	
+	
+
+	// Change Image
+	function changeImg(){
+		document.slide.src = images[i];
+
+		if(i < images.length - 1){
+			i++;
+		} else {
+			i = 0;
+		}
+
+		setTimeout("changeImg()", time);
+	}
+
+	window.onload = changeImg;
+
+</script>
+
+
+</body>
+</html>
